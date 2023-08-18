9a1d45560e40a814887b0fba97806bf8f1a3106a
t-jimbo <jbear.um@gmail.com>
2023-08-18 13:15:59 +0900

    refactor: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 // Import React and ReactDOM
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './components/app.jsx';
 
 // Import Framework7
 import Framework7 from 'framework7/lite-bundle';
@@ -12,15 +13,16 @@ import Framework7React from 'framework7-react';
 import 'framework7/css/bundle';
 
 // Import Icons and App Custom Styles
-import '../css/icons.css';
-import '../css/app.less';
-
-// Import App Component
-import App from '../components/app.jsx';
+import './css/icons.css';
+import './css/app.less';
 
 // Init F7 React Plugin
 Framework7.use(Framework7React)
 
 // Mount React App
 const root = createRoot(document.getElementById('app'));
-root.render(React.createElement(App));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
