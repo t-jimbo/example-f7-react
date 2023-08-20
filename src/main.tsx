09e3b022a37453c564e183efbe5da5a487b3925d
t-jimbo <jbear.um@gmail.com>
2023-08-20 15:51:04 +0900

    refactor: app移動

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
 // Import React and ReactDOM
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/app.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app.jsx";
 
 // Import Framework7
-import Framework7 from 'framework7/lite-bundle';
+import Framework7 from "framework7/lite-bundle";
 
 // Import Framework7-React Plugin
-import Framework7React from 'framework7-react';
+import Framework7React from "framework7-react";
 
 // Import Framework7 Styles
-import 'framework7/css/bundle';
+import "framework7/css/bundle";
 
 // Import Icons and App Custom Styles
-import './css/icons.css';
-import './css/app.less';
+import "./css/icons.css";
+import "./css/app.less";
 
 // Init F7 React Plugin
-Framework7.use(Framework7React)
+Framework7.use(Framework7React);
 
 // Mount React App
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
